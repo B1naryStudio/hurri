21abaebdc6f62da537c45fd69164589342c136db
Mykyta Semenistyi <nikeiwe@gmail.com>
2014-08-02 09:27:42 +0300

    change routes setup

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,12 @@
 var express = require('express');
-var socketio = require('socket.io');
 
 var path = require('path');
 
 var app = express();
-var server = require('http').Server(app);
-var io = socketio(server);
 
 var staticPath = path.normalize(__dirname + '/../public');
 app.use(express.static(staticPath));
 
-io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-  	//dostuff
-  });
-});
+var routes = require('api/routes')(app);
 
 app.listen(3055);
