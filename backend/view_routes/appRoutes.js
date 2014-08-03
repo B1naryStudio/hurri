211e10b8b90dad0ada53fb8aa0091a59d08dcd14
BogdanRusinka <rusinka.bogdann@mail.ru>
2014-08-03 14:29:39 +0300

    Mongo connection : Add dump. Add connection in appRoutes

--- backend/view_routes/appRoutes.js
+++ backend/view_routes/appRoutes.js
@@ -1,5 +1,15 @@
+var MongoClient = require('mongodb').MongoClient;
+
+
 module.exports = function(app){
 	app.get('/', function(req, res, next){
-		res.render('index');
+		MongoClient.connect('mongodb://localhost:27017/hurri', function(err, db) {
+ 		 if(!err) {
+ 		 	db.collection('user_auth').find({name : 'Nikita'}).each(function(err, results) {
+        		res.send(results.friends);
+      		});
+  			} else 
+  			res.send('Not works DB');
+});
 	});
 };
