f189a3b3173a2c35cfa79b11f85b4d2a9d8c855f
BogdanRusinka <rusinka.bogdann@mail.ru>
2014-08-04 22:56:48 +0300

    Config: Updated files

---
+++ backend/db/dbconnect.js
@@ -0,0 +1,32 @@
+function dbConnectionHandler(){
+var mongoose = require( 'mongoose' );
+var config = require('../config/');
+console.log(config.db.opts.user);
+mongoose.connect(config.db.uri, config.db.opts);
+
+mongoose.connection.on('connected', function () {
+	this.state = 'connected';
+  	console.log('Mongoose default connection open to ' + config.db.uri);
+});
+
+mongoose.connection.on('error',function (err) {
+	this.state = 'disconnected';
+ 	console.log('Mongoose default connection error: ' + err);
+});
+
+mongoose.connection.on('disconnected', function () {
+	this.state = 'disconnected';
+  	console.log('Mongoose default connection disconnected');
+});
+
+process.on('SIGINT', function() {
+  mongoose.connection.close(function () {
+  	this.state = 'disconnected';
+    console.log('Mongoose default connection disconnected through app termination');
+    process.exit(0);
+  });
+});
+
+}
+
+module.exports = new dbConnectionHandler();
