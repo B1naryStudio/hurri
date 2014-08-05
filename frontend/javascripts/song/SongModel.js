dc1c7d1b0aa2441079aeb59186eea1a30ae761f5
Snakedn <snake.dn@yandex.ru>
2014-08-05 16:51:56 +0300

    fix SongModel structure

--- frontend/javascripts/song/SongModel.js
+++ frontend/javascripts/song/SongModel.js
@@ -3,26 +3,15 @@ var SongModel = Backbone.Model.extend({
 	defaults:{
 		title : Title,
 		duration : undefined,
-		position : {
-		type: 0
-	    },
-	    release_date : Date,
-	    kbps : {
-	        type: 128},
-	    lyrics : {
-	        type: 'No lyrics for this song. Sorry.'
-	    },
-	    album : {
-	        type : undefined,
-	        ref : 'Album'
-	    },
-	    singer : {
-	        type : undefined,
-	        ref : 'Singer'
-	    },
+		position : 0,
+	    release_date : new Date(1),
+	    kbps : 128,
+	    lyrics : 'No lyrics for this song. Sorry.',
+	    albumTitle : 'AlbumName',
+		albumCover: 'cover.jpg',
+	    singerName: 'Singer',
 	    url : 'demo.mp3',
 	    comments : ['']
-
 	}
 });
 
