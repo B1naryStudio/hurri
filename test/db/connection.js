ec23656d16bf116467ba7082a38835b8df3c7c94
BogdanRusinka <rusinka.bogdann@mail.ru>
2014-08-06 18:49:40 +0300

    DB: Add connection tests

--- test/db/connection.js
+++ test/db/connection.js
@@ -1,14 +1,54 @@
-var mongoose = require('mongoose');
-var mockgoose = require('mockgoose');
+describe('Connection Tests', function () {
+	var Mongoose = require('mongoose').Mongoose;
+	var mockgoose = require('mockgoose');
+	var mongoose;
 
-mockgoose(mongoose);
-
-describe('Connect', function () {
-	it('Should NOT return an error when connecting to Mockgoose through connect', function (done) {
-        mongoose.connect('mongodb://localhost:27017/hurri', function (err, result) {
-                expect(err).toBeNull();
-                expect(result).toBeTruthy();
-                done();
-        });
+	beforeEach(function (done) {
+        mongoose = new Mongoose();
+        mockgoose(mongoose);
+        done();
     });
+
+	describe('Connect', function () {
+
+		it('Should NOT return an error when connecting to Mockgoose through connect', function (done) {
+	        mongoose.connect('mongodb://localhost:27017/hurri', function (err, result) {
+	                (err === null).should.be.true;
+	                result.should.be.ok;
+	                done();
+	        });
+	    });
+
+	    it('Be able to connect with just a host and database', function (done) {
+            (function () {
+                mongoose.connect('mongodb://localhost:27017/', 'hurri');
+                done();
+            }).should.not.throw();
+        }); 
+
+	});
+
+	describe('Events', function () {
+
+		it('Dispatch connecting event on connect', function (done) {
+            var connection = mongoose.connect('mongodb://localhost:27017/hurri').connection;
+            connection.on('connecting', function () {
+                done();
+            });
+        });
+
+        it('Dispatch Error event on connect', function (done) {
+            mongoose = new Mongoose();
+            mockgoose(mongoose, true);
+            var connection = mongoose.connect('mongodb://localhost:27017/hurri').connection;
+            connection.on('error', function (err) {
+                err.should.be.ok;
+                done();
+            });
+        });
+
+	});
+
+
+
 });
