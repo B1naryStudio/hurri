813c5c244168e056a350347cb324433c8a1f236c
Miroslav Martynov <marty.mircha@gmail.com>
2014-08-06 09:02:05 +0300

    Prettified JSON string on render template.:feelsgood:

--- backend/view_routes/renderHelper.js
+++ backend/view_routes/renderHelper.js
@@ -7,6 +7,6 @@ var app = express();
 module.exports = function (params) {
 
 	var file = fs.readFileSync(__dirname + '/../../public/' + 'index.html', 'utf8');
-	return file.replace(/<\!\-\-data_replace\-\-\>/g, JSON.stringify(params.data));
+	return file.replace(/<\!\-\-data_replace\-\-\>/g, JSON.stringify(params.data,null,'\t'));
 
 };
