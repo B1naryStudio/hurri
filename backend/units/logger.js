06e835910095a2c4fac5b607f79bca1e44ce576d
Miroslav Martynov <marty.mircha@gmail.com>
2014-08-07 17:18:44 +0300

    Added logs config.

--- backend/units/logger.js
+++ backend/units/logger.js
@@ -1,15 +1,16 @@
 var winston = require('winston');
+var config = require('../config/');
 
 var logger = new (winston.Logger)({
 	transports: [
-		new (winston.transports.Console)({ json: false, timestamp: true }),
-		new winston.transports.File({ filename: __dirname + '/../../logs/common/development.log', json: false })
+		new (winston.transports.Console)({ json: config.logger.transports.console.json, timestamp: config.logger.transports.console.timestamp }),
+		new winston.transports.File({ filename: config.logger.transports.file.filename, json: config.logger.transports.file.json })
 	],
 	exceptionHandlers: [
-		new (winston.transports.Console)({ json: true, timestamp: true }),
-		new winston.transports.File({ filename: __dirname + '/../../logs/common/error.log', json: false })
+		new (winston.transports.Console)({ json: false, timestamp: true }),
+		new winston.transports.File({ filename: config.logger.exceptionHandlers.file.filename, json: config.logger.exceptionHandlers.file.json })
 	],
-	exitOnError: true
+	exitOnError: config.logger.exitOnError
 });
 
 module.exports = logger;
