e5501fc7c16d6d619b2b48cff8dbe99eedd7930a
BogdanRusinka <rusinka.bogdann@mail.ru>
2014-08-08 11:36:34 +0300

    MusicInfoWrapper: Initial commit. Add file. Update Wrapper

--- backend/info_service_wrappers/DeezerWrapper.js
+++ backend/info_service_wrappers/DeezerWrapper.js
@@ -1,11 +1,17 @@
+var request = require('request');
 function DeezerWrapper(){
 
 }
 
-DeezerWrapper.prototype.getArtistByName = function(){
+DeezerWrapper.prototype.getArtistById = function(){
 };
 
-DeezerWrapper.prototype.getAlbumByName = function(){
+DeezerWrapper.prototype.getAlbums = function(id){
+	request('http://api.deezer.com/album/'+id, function (error, response, body) {
+  	if (!error && response.statusCode == 200) {
+    	console.log(body);
+  	}
+})
 };
 
 module.exports = DeezerWrapper;
