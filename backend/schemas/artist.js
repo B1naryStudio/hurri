cecbadc183da1c40e5d3442befe946af6a420887
BogdanRusinka <rusinka.bogdann@mail.ru>
2014-08-08 20:38:08 +0300

    Deezer Wrapper: Update schemas and Repositories

--- backend/schemas/artist.js
+++ backend/schemas/artist.js
@@ -3,15 +3,13 @@ var Schema = mongoose.Schema;
 var albumModel = require('./album.js');
 
 var artistSchema = new Schema({
+    deezer_id : Number,
     name : String,
     picture : {
         type: String, 
         default: '/image/defaultSinger.jpg'
     },
-    albums_id : [{ 
-		type: Schema.Types.ObjectId, 
-		ref: 'Album'
-	}],
+    albums_id : [Number],
     genres : [String],
     bio : {
         type: String, 
