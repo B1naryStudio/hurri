8cc6509c1b0ed8cb3e18419624fd8570e984f58a
msemenistyi <nikeiwe@gmail.com>
2014-08-14 10:54:11 +0300

    vk-wrapper: fix tests

--- backend/social_network_wrapper/VKWrapper.js
+++ backend/social_network_wrapper/VKWrapper.js
@@ -3,29 +3,31 @@ var VK = require('vksdk');
 
 
 function VKWrapper(){
+
 	this.vk = new VK({
 		'appID'	: 4504196,
 		'appSecret' : 'sKTyZBHIHu17JeRLTsez',
 		'mode'      : 'oauth'
 	});
 	this.vk.setToken( { token :'07cf157b9f176abb17cfd2f25b9868d5bd8e3eaa1756d0d258ceda52f9cb6d7e817d87bc00da960440adc' });
+
 }
 
-VKWrapper.prototype.getUserInfo= function(id, fields, callback){
+VKWrapper.prototype.getUserInfo = function(id, fields, callback){
 	this.vk.request('users.get', {'uids' : id, 'fields': fields});
 	this.vk.on('done:users.get', function(result) {
 		callback(result);
 	});
 };
 
-VKWrapper.prototype.getFriends= function(id, callback){
+VKWrapper.prototype.getFriends = function(id, callback){
 	this.vk.request('friends.get', {'uids' : id});
 	this.vk.on('done:friends.get', function(result) {
 		callback(result);
 	});
 };
 
-VKWrapper.prototype.getOnlineFriends= function(id, callback){
+VKWrapper.prototype.getOnlineFriends = function(id, callback){
 	this.vk.request('friends.getOnline', {'uids' : id});
 	this.vk.on('done:friends.getOnline', function(result) {
 		callback(result);
@@ -33,35 +35,40 @@ VKWrapper.prototype.getOnlineFriends= function(id, callback){
 };
 
 VKWrapper.prototype.getUserAudio = function(id, callback){
-	this.vk.request('audio.get', {'owner_id' : id});
-	this.vk.on('done:audio.get', function(result) {
+	this.vk.request('audio.get', {'owner_id' : id}, 'get-user-audio');
+	this.vk.on('get-user-audio', function(result) {
 		callback(result);
 	});
 };
 
 VKWrapper.prototype.getAudioById = function(aid, count, callback){
-	this.vk.request('audio.get', {'audio_ids' : aid, 'count': count});
-	this.vk.on('done:audio.get', function(result) {
+	this.vk.request('audio.get', {'audio_ids' : aid, 'count': count}, 'get-audio-by-id');
+	this.vk.on('get-audio-by-id', function(result) {
 		callback(result);
 	});
 };
 
 VKWrapper.prototype.getLyricsById = function(lid, count, callback){
 	this.vk.request('audio.getLyrics', {'lyrics_id' : lid, 'count': count});
-	this.vk.on('done: audio.getLyrics', function(result) {
+	this.vk.on('done:audio.getLyrics', function(result) {
 		callback(result);
 	});
 };
-VKWrapper.prototype.getAudioSearch = function(query, sort, onlyArtist, auto_complete, count, callback){
-	this.vk.request('audio.search', {'q' : query, 'sort': sort, 'performer_only': onlyArtist, 'auto_complete': auto_complete, 'count': count});
+
+VKWrapper.prototype.getAudioSearch = function(options, callback){
+
+	this.vk.request('audio.search', {
+		'q' : options.query, 
+		'sort': options.sort, 
+		'performer_only': options.onlyArtist, 
+		'auto_complete': options.auto_complete, 
+		'count': options.count
+	});
+	
 	this.vk.on('done:audio.search', function(result) {
-		console.log(result);
 		callback(result);
 	});
 
-	return 1;
 };
-
-
 
 module.exports = new VKWrapper();
--- test/wrappers/VKWrapper.js
+++ test/wrappers/VKWrapper.js
@@ -14,54 +14,64 @@ describe('VKWrapper should', function(){
 });
 
 describe('VKWrapper functions should', function(){
+
 	it('getUserInfo returns object with necessary data', function(done){
 		VK.getUserInfo('18252726','photo_50,city',function(result){
 			result.should.be.Object;
-			console.log(result);
 			done();
 		});
 	});
+
 	it('getFriends returns object with necessary data', function(done){
 		VK.getFriends('18252726', function(result){
 			result.should.be.Object;
-			console.log(result);
 			done();
 		});
 	});
+
 	it('getFriendsOnline returns object with necessary data', function(done){
 		VK.getOnlineFriends('18252726', function(result){
 			result.should.be.Object;
-			console.log(result);
 			done();
 		});
 	});
+
 	it('getUserAudio returns object with necessary data', function(done){
 		VK.getUserAudio('18252726', function(result){
 			result.should.be.Object;
-			console.log(result);
 			done();
 		});
 	});
+
 	it('getAudioById returns object with necessary data', function(done){
 		VK.getAudioById('18252726_295129112', 3, function(result){
 			result.should.be.Object;
-			console.log(result);
 			done();
 		});
 	});
+
 	it('getLyricsById returns object with necessary data', function(done){
 		VK.getLyricsById('2428970', 3, function(result){
 			result.should.be.Object;
-			console.log(result);
 			done();
 		});
 	});
+
 	it('getAudioSearch returns object with necessary data', function(done){
-		VK.getAudioSearch('Yesterday', 2, 1, 1, 3, function(result){
+		
+		var options = {
+			query: 'Yesterday',
+			sort: 2,
+			onlyArtist: 1,
+			auto_complete: 1,
+			count: 3
+		};
+
+		VK.getAudioSearch(options, function(result){
 			result.should.be.Object;
-			console.log("WARNING!!")
-			console.log(result);
 			done();
 		});
+
 	});
+
 });
