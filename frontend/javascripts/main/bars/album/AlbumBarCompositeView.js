8356d3ffb0fa2176aa41d009754fc3aca5e99255
BogdanRusinka <rusinka.bogdann@mail.ru>
2014-08-14 17:06:39 +0300

    Main composite: Add handlers

--- frontend/javascripts/main/bars/album/AlbumBarCompositeView.js
+++ frontend/javascripts/main/bars/album/AlbumBarCompositeView.js
@@ -2,7 +2,23 @@ define(['marionette', './AlbumBarView', '../app/context'], function(Marionette, 
 	var AlbumBarCompositeView = Marionette.CompositeView.extend({
 		id: '#album-bar-wrapper',
 		childView: AlbumBarView,
-		template: '#album-bar-composite'
+		template: '#album-bar-composite',
+		events {
+			'click #album-select' : 'albumShow',
+			'click #artist-select' : 'artistShow',
+			'click #track-select' : 'trackShow'
+		},
+		albumShow : function(){
+			Backbone.trigger('show-albums');
+		},
+
+		artistShow : function(){
+			Backbone.trigger('show-artists');
+		},
+
+		trackShow : function(){
+			Backbone.trigger('show-tracks');
+		}
 	});
 	return AlbumBarCompositeView;
 });
