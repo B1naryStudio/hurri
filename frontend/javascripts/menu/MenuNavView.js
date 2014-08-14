f2a8ecf5f4be4e1fb934dedf5ed1d75c442b7e6c
BogdanRusinka <rusinka.bogdann@mail.ru>
2014-08-14 14:34:21 +0300

    Menu: Add styles and handlers

--- frontend/javascripts/menu/MenuNavView.js
+++ frontend/javascripts/menu/MenuNavView.js
@@ -1,7 +1,44 @@
 define(['marionette'], function(Marionette){
 	MenuNavView = Marionette.ItemView.extend({
-		template: '#menu-nav-template'
-	});
+		template: '#menu-nav-template',
+		events: {
+			'click .favourites-image' : 'showFavorites',
+			'click .show-playlists' : 'showPlaylists',
+			'click .show-groupes' : 'showGroupes',
+			'click .show-by-genres' : 'showGenres',
+			'click .show-by-artists' : 'showArtists',
+			'click .show-by-albums' : 'showAlbums',
+			'click .show-by-charts' : 'showCharts'
+		},
 
+		showFavorites : function(){
+			Backbone.trigger('show-favorites');
+		},
+
+		showPlaylists : function(){
+			Backbone.trigger('show-playlists');
+		},
+
+		showGroupes : function(){
+			Backbone.trigger('show-groupes');
+		},
+
+		showGenres : function(){
+			Backbone.trigger('show-genres');
+		},
+
+		showArtists : function(){
+			Backbone.trigger('show-artists');
+		},
+
+		showAlbums : function(){
+			Backbone.trigger('show-albums');
+		},
+
+		showCharts : function(){
+			Backbone.trigger('show-charts');
+		}
+	});
+	
 	return MenuNavView;
 });
