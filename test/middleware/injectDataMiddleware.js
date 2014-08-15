b6586887b3ce8cd7652165019a6e3ef32f95efde
Miroslav Martynov <marty.mircha@gmail.com>
2014-08-15 08:13:42 +0300

    Fixed data inject test :clap:

--- test/middleware/injectDataMiddleware.js
+++ test/middleware/injectDataMiddleware.js
@@ -1,22 +1,26 @@
 var app = require('../../backend/server');
-var request = require('request');
+var request = require('supertest');
 var should = require('should');
 var injectData = require('../../backend/middleware/injectDataMiddleware');
 
 describe('injectDataMiddleware should', function () {
 		it('set global variable _is404Error and respond with 200 status if route is not found', function (done) {
-			request.get('http://localhost:3055/sdasd', function(error, res, body) {
+			request(app)
+				.get('/sdasd')
+				.end(function(err, res) {
 					res.statusCode.should.equal(200);
-					body.should.match(/window\._is404Error = true;/);
-					done();
+					res.text.should.match(/window\._is404Error = true;/);
+					done(err);
 				})
 		});
 
 		it('leave variable _is404Error to false and respond with 200 status if route is known', function (done) {
-			request.get('http://localhost:3055/album/1', function(error, res, body) {
+			request(app)
+				.get('/album/1')
+				.end( function(err, res) {
 					res.statusCode.should.equal(200);
-					body.should.match(/window\._is404Error = false;/);
-					done();
+					res.text.should.match(/window\._is404Error = false;/);
+					done(err);
 				})
 		});
 });
