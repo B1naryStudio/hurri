0bfab252d327c720b0d710ae74f3e88fb4c99297
Miroslav Martynov <marty.mircha@gmail.com>
2014-08-15 10:38:18 +0300

    Added test for 404 error text.

--- test/view_routes/commonRoutes.js
+++ test/view_routes/commonRoutes.js
@@ -1,36 +1,49 @@
 var app = require('../../backend/server');
 var request = require('supertest');
+var should = require('should');
 
-describe('view_routes should', function(){
+describe('view_routes should', function () {
 
-  it('have get / route', function(done){
-    request(app)
-      .get('/')
-      .expect(200)
-      .expect('Content-Type', /text\/html/)
-      .end(function(err, res){
-        done(err);
-      })
-  });
+	it('has get / route', function (done) {
+		request(app)
+			.get('/')
+			.expect(200)
+			.expect('Content-Type', /text\/html/)
+			.end(function (err, res) {
+				done(err);
+			})
+	});
 
-  it('have get /404 route', function(done){
-    request(app)
-      .get('/404')
-      .expect(200)
-      .expect('Content-Type', /text\/html/)
-      .end(function(err, res){
-        done(err);
-      })
-  });
+	it('has get /404 route', function (done) {
+		request(app)
+			.get('/404')
+			.expect(200)
+			.expect('Content-Type', /text\/html/)
+			.end(function (err, res) {
+				done(err);
+			})
+	});
 
-  it('have get /signin route', function(done){
-    request(app)
-      .get('/signin')
-      .expect(200)
-      .expect('Content-Type', /text\/html/)
-      .end(function(err, res){
-        done(err);
-      })
-  });
+	it('has get /signin route', function (done) {
+		request(app)
+			.get('/signin')
+			.expect(200)
+			.expect('Content-Type', /text\/html/)
+			.end(function (err, res) {
+				done(err);
+			})
+	});
+
+
+	it('has NotFound error text "Page not found"', function (done) {
+		request(app)
+			.get('/404')
+			.expect(200)
+			.expect('Content-Type', /text\/html/)
+			.end(function (err, res) {
+				res.text.should.match(/Page not found/);
+				done(err);
+			})
+	});
 
 });
