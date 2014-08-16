5d4afcdba24d32c54eb50c5f709bcc4d855cd644
BogdanRusinka <rusinka.bogdann@mail.ru>
2014-08-16 23:29:46 +0300

    Route fixes. Album view update

--- frontend/javascripts/main/bars/album/AlbumBarView.js
+++ frontend/javascripts/main/bars/album/AlbumBarView.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-define(['marionette', 'app/context'], function(Marionette, context){
-=======
 define(['marionette', '../../../app/context'], function(Marionette, context){
->>>>>>> player-enhancements
 	var AlbumBarView = Marionette.ItemView.extend({
 		className: 'album-bar',
   		template : '#album-bar'
