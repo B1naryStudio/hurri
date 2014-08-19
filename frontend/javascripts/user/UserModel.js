4511432b3fdcaf61f19ab2df04dcd0e9ef0e827f
Artem Nosach <artem.nosach@binary-studio.com>
2014-08-19 21:07:13 +0300

    Add new fields to user model.

--- frontend/javascripts/user/UserModel.js
+++ frontend/javascripts/user/UserModel.js
@@ -11,7 +11,9 @@ define(['backbone'], function(Backbone){
 			//
 			liked: undefined,
 			playlists: undefined,
-			group: undefined,
+			groups: undefined,
+			listened: undefined,
+			followers: undefined
 		},
 		
 		initialize: function(){
@@ -25,6 +27,12 @@ define(['backbone'], function(Backbone){
 			this.set('age', 60);
 			this.set('email', 'vincent-vega@gmail.com');
 			this.set('country', 'United States');
+			//
+			this.set('liked', ['like1', 'like2', 'like3']);
+			this.set('playlists', ['playlist1', 'playlist2', 'playlist3']);
+			this.set('groups', ['group1', 'group2', 'group3']);
+			this.set('listened', 126);
+			this.set('followers', ['follower1', 'follower2', 'follower3']);
 		},
 
 	});
