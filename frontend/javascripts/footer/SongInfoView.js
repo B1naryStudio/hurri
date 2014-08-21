fb98b73596e214b8a92dbd137cc960afcff1ae12
Miroslav Martynov <marty.mircha@gmail.com>
2014-08-21 09:19:50 +0300

    Added scrolling title

--- frontend/javascripts/footer/SongInfoView.js
+++ frontend/javascripts/footer/SongInfoView.js
@@ -6,7 +6,21 @@ define(['marionette'], function(Marionette){
 		},
 
 		fieldsChanged: function() {
+			clearInterval(this.titleInterval);
+			this.tabTitle(this.model.attributes.artist + " — " + this.model.attributes.title);
 			this.render();
+		},
+
+		tabTitle: function(text) {
+			text = text || 'Hurri';
+			var _text = " ::: " + text;
+			var title = $('head > title');
+			this.titleInterval = setInterval(function() {
+				var first = _text.substr(0,1);
+				_text = _text.substr(1);
+				_text += first;
+				title.text(_text);
+			}, 500);
 		}
 	});
 	return SongInfoView;
