fd0c9e7546ba47fc19f383886bc5a44096c876c6
Miroslav Martynov <marty.mircha@gmail.com>
2014-08-22 08:58:14 +0300

    Trying catch close tab event and set active tab to localStore.

--- frontend/javascripts/player/PlayerView.js
+++ frontend/javascripts/player/PlayerView.js
@@ -23,7 +23,7 @@ define(['marionette', './PlayerModel'], function(Marionette, PlayerModel){
 			"mousedown @ui.playbackRange"	: "setUpPlayback",
 			"mouseout @ui.playbackRange" 	: "resetPlaybackMouseUpFlag",
 			"mouseout @ui.volumeRange" 		: "resetVolumeMouseUpFlag",
-			"mouseup @ui.playbackRange" 	: "resetPlaybackMouseUpFlag",
+			"mouseup @ui.playbackRange" 	: "resetPlaybackMouseUpFlag"
 		},
 		ui: {
    			nextButton: "#next-button",
@@ -38,7 +38,7 @@ define(['marionette', './PlayerModel'], function(Marionette, PlayerModel){
    			volume: '#volume',
    			player : '#player'
   		},
-  		
+
   		setUpVolume: function(){
   			this.flag = true;
   			this.volumeLevelSetup();
@@ -66,10 +66,15 @@ define(['marionette', './PlayerModel'], function(Marionette, PlayerModel){
 			this.model.nextTrack();
 		},
 		playbackState : function(){
-			var mode = this.model.playbackState();
-			mode = 'player-button' + ' ' + mode;
-			this.ui.playButton.removeClass();
-			this.ui.playButton.addClass(mode);
+			if (this.isMainTab()) {
+				window.localStorage.setItem('mainTab', window.sessionStorage.tabId);
+				var mode = this.model.playbackState();
+				mode = 'player-button' + ' ' + mode;
+				this.ui.playButton.removeClass();
+				this.ui.playButton.addClass(mode);
+			} else {
+				console.log('This tab isn\'t main');
+			}
 		},
 
 		previousTrack : function(){	
@@ -119,6 +124,31 @@ define(['marionette', './PlayerModel'], function(Marionette, PlayerModel){
 				this.model.playbackPosition(input);	
 			}	
 		},
-	}); 
+		generateTabId: function() {
+			return Math.floor((1 + Math.random()) * 0x10000)
+				.toString(16)
+				.substring(1);
+		},
+
+		isMainTab: function() {
+			var thisTab = window.sessionStorage.tabId;
+			var mainTab = window.localStorage.getItem('mainTab');
+			if (mainTab === 'undefined' || mainTab === null || mainTab.length === 0) {
+				return true;
+			} else {
+				return mainTab === thisTab;
+			}
+
+		}
+	});
+
+	this.onbeforeunload = function (e) {
+		alert('asdasd');
+		if (this.isMainTab()) {
+			this.localStorage.removeItem('mainTab');
+		}
+		this.sessionStorage.tabId = PlayerView.generateTabId();
+	};
+
 	return PlayerView;
 });
