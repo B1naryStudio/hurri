7046dfaa75e3316012a70a58fec8c0eda7431074
Miroslav Martynov <marty.mircha@gmail.com>
2014-08-22 09:39:07 +0300

    Layout fixes

--- frontend/javascripts/main/radio/RadioCollectionView.js
+++ frontend/javascripts/main/radio/RadioCollectionView.js
@@ -1,6 +1,7 @@
 define(['marionette', './RadioView', '../../app/context'], function(Marionette, RadioView, context){
 	var RadioCollectionView = Marionette.CollectionView.extend({
-		childView: RadioView
+		childView: RadioView,
+		className: 'content'
 	});
 	return RadioCollectionView;
 });
