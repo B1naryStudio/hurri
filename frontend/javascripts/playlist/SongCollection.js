f0638ea94d84512d9a09ed8a96799767818793de
Snakedn <snake.dn@yandex.ru>
2014-08-27 12:05:18 +0300

    prepare to merge

--- frontend/javascripts/playlist/SongCollection.js
+++ frontend/javascripts/playlist/SongCollection.js
@@ -1,12 +1,10 @@
 define(['backbone', '../song/SongModel'], function(Backbone, SongModel){
 var SongCollection = Backbone.Collection.extend({
 	model: SongModel,
-	comparator: 'order'
+	comparator: 'order',
 });
 
 songCollection = new SongCollection();
-
-
 
 songCollection.add([
 	{title: 'The Moment (Bootleg Edit)', artist: 'Nick Kech feat. Madilyn Bailey', duration:280}
