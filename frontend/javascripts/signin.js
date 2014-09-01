2758ad931691baf40183ec181fdc09727f3663e1
Snakedn <snake.dn@yandex.ru>
2014-09-01 16:21:17 +0300

    signin: delete excessive dependencies

---
+++ frontend/javascripts/signin.js
@@ -0,0 +1,9 @@
+require.config({
+	baseUrl: '/javascripts',
+	paths: {
+		jquery: '../../bower_components/jquery/dist/jquery.min',
+		underscore: '../../bower_components/underscore/underscore'
+	}
+});
+
+require(['./blur']);
