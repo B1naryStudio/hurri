ecd4a651cbf161d11689c79c9f6271a4bff5f2eb
BogdanRusinka <rusinka.bogdann@mail.ru>
2014-09-10 14:40:04 +0300

    Search result: Add different names to classes

--- frontend/javascripts/header/searchbar/searchresults/SearchResultsView.js
+++ frontend/javascripts/header/searchbar/searchresults/SearchResultsView.js
@@ -7,31 +7,46 @@ define(['marionette', '../../../app/routes'], function(Marionette, router){
 
 
 		ui: {
-			resultsRegion: '.searchresults-region',
+			resultsRegionAlbum: '.searchresults-region_album',
+			resultsRegionArtist: '.searchresults-region_artist',
+			resultsRegionSong: '.searchresults-region_song',
 			more 		: '.show-more-results'
   		},
 
 		events: {
-			'click @ui.resultsRegion': 'select',
+			'click @ui.resultsRegionAlbum': 'showAlbum',
+			'click @ui.resultsRegionArtist': 'showArtist',
+			'click @ui.resultsRegionSong': 'showSong',
 			'click .show-more-results' : 'showMore'
 		},
 
 		// showMore : function(){
 		// 	router.navigate('/search/' + this.ui.searchInput[0].value, true);
 		// }, 
+		showAlbum: function(){
+			router.navigate('/album/id/' + this.collection.model._id, true);
+		},
+
+		showSong: function(){
+			router.navigate('/track/id/'+ context.currentSongModel.attributes._id,true);
+		},
+
+		showArtist: function(){
+			router.navigate('/artist/id/' + this.model.attributes._id + '/albums', true);
+		},
 
 		attachHtml: function(compositeView, childView, index){
 			switch(childView.model.get('type')){
 				case 'album':
-					compositeView.ui.resultsRegion[0].appendChild(childView.el);
+					compositeView.ui.resultsRegionAlbum[0].appendChild(childView.el);
 					break;
 
 				case 'artist':
-					compositeView.ui.resultsRegion[1].appendChild(childView.el);
+					compositeView.ui.resultsRegionArtist[0].appendChild(childView.el);
 					break;
 
 				case 'song':
-					compositeView.ui.resultsRegion[2].appendChild(childView.el);
+					compositeView.ui.resultsRegionSong[0].appendChild(childView.el);
 					break;
 			}
 			
