c203767aab18edaa682b6a587414876cbb6f3864
Snakedn <snake.dn@yandex.ru>
2014-09-15 20:45:56 +0300

    charts: backend
    Billboard+iTunes+BBC

--- backend/info_service_wrappers/xmlParser.js
+++ backend/info_service_wrappers/xmlParser.js
@@ -1,12 +1,48 @@
-var fs = require('fs'),
-    xml2js = require('xml2js');
+var request = require('request');
+var fs = require('fs');
+var xml2js = require('xml2js');
+var parseString = require('xml2js').parseString;
 
-var parser = new xml2js.Parser();
-parser.addListener('end', function(result) {
-    console.dir(result);
-    console.log('Done.');
-});
-fs.readFile('http://www.billboard.com/rss/charts/hot-100', function(err, data) {
-    parser.parseString(data);
-});
-module.exports = new xmlParser();
+function chartParse(){
+
+}
+
+chartParse.prototype.billboardTop = function (callback) {
+	var baseURL = 'http://www.billboard.com/rss/charts/hot-100';
+	request(baseURL , function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			parseString(body, function (err, result) {
+				console.log('parse string=', result.rss.channel[0].item);
+				console.log('parse err=', err);
+				callback(result.rss.channel[0].item);
+			});
+		}
+	});
+};
+
+chartParse.prototype.benMajorUkTop = function (callback) {
+	var baseURL = 'http://ben-major.co.uk/labs/top40/api/singles/';
+	request(baseURL , function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			console.log('body ben-major=', body);
+			var result = JSON.parse(body);
+			callback(result.entries);
+		}
+	});
+};
+
+chartParse.prototype.iTunesTop = function (callback) {
+	var baseURL = 'https://itunes.apple.com/ua/rss/topsongs/limit=25/explicit=true/xml';
+	request(baseURL , function (error, response, body) {
+		parseString(body, function (err, result) {
+			console.log('parse string=', result.feed.entry);
+			console.log('parse err=', err);
+			callback(result.feed.entry);
+		});
+	});
+};
+
+module.exports = new chartParse();
+
+
+
