db1fe37244d4c93c128570ce33a0f47e64098e25
BogdanRusinka <rusinka.bogdann@mail.ru>
2014-09-26 22:02:47 +0300

    Sitetour styled

--- frontend/javascripts/sitetour/SitetourView.js
+++ frontend/javascripts/sitetour/SitetourView.js
@@ -33,7 +33,7 @@ define(['marionette'], function(Marionette){
 			switch(this.stage) {
 				case 0:
 					this.ui.arrow[0].style.display = 'none';
-					this.ui.info[0].innerHTML = 'Welcome to Hurri!<br>Hurri is a music service for true melomans.<br>' +
+					this.ui.info[0].innerHTML = '<h3 class="tour">Welcome to Hurri!</h3><br>Hurri is a music service for true melomans.<br>' +
 												'Listen to your favorite music, follow last musical novelties,<br>' +
 												'get friends and share your musical preferences.';
 					this.ui.info[0].style.display = 'block';
@@ -46,7 +46,7 @@ define(['marionette'], function(Marionette){
 					this.ui.arrow[0].style.left = '300px';
 					this.ui.arrow[0].style.top = '80px';
 					this.ui.arrow[0].style.transform = 'rotate(-90deg)';
-					this.ui.info[0].innerHTML = 'Play music with usable and fully-featured<br>' +
+					this.ui.info[0].innerHTML = '<h3 class="tour">Player</h3><br>Play music with usable and fully-featured<br>' +
 												'musical player. Try all playback modes and effects<br>' +
 												'with easy and understandable interface.';
 					this.ui.info[0].style.display = 'block';
@@ -59,7 +59,7 @@ define(['marionette'], function(Marionette){
 					this.ui.arrow[0].style.left = '150px';
 					this.ui.arrow[0].style.top = '200px';
 					this.ui.arrow[0].style.transform = 'rotate(-90deg)';
-					this.ui.info[0].innerHTML = 'Use menu to navigate the Hurri.<br>' +
+					this.ui.info[0].innerHTML = '<h3 class="tour">Menu</h3><br>Use menu to navigate the Hurri.<br>' +
 												'Explore music, view charts and listen to radio.<br>' +
 												'Also, you can find your favorite songs here.';
 					this.ui.info[0].style.display = 'block';
@@ -72,7 +72,7 @@ define(['marionette'], function(Marionette){
 					this.ui.arrow[0].style.left = '150px';
 					this.ui.arrow[0].style.top = '350px';
 					this.ui.arrow[0].style.transform = 'rotate(-90deg)';
-					this.ui.info[0].innerHTML = 'Manage your playlists with "playlists" panel.<br>' + 
+					this.ui.info[0].innerHTML = '<h3 class="tour">Playlists</h3><br>Manage your playlists with "playlists" panel.<br>' + 
 												'Create and edit your own playlists to listen<br>' + 
 												'only the best music all the time.';
 					this.ui.info[0].style.display = 'block';
@@ -85,7 +85,7 @@ define(['marionette'], function(Marionette){
 					this.ui.arrow[0].style.left = $(document).width() - 300 + 'px';
 					this.ui.arrow[0].style.top = $(document).height() - 100 + 'px';
 					this.ui.arrow[0].style.transform = 'rotate(90deg)';
-					this.ui.info[0].innerHTML = 'On the sidebar you will see current<br>' +
+					this.ui.info[0].innerHTML = '<h3 class="tour">Sidebar</h3><br>On the sidebar you will see current<br>' +
 												'playing songs queue. You can edit it as you like.<br>' +
 												'Your followers, followings and account statistic<br>' +
 												'could be founded here.';
@@ -99,7 +99,7 @@ define(['marionette'], function(Marionette){
 					this.ui.arrow[0].style.left = $(document).width() - 300 + 'px';
 					this.ui.arrow[0].style.top = '30px';
 					this.ui.arrow[0].style.transform = 'rotate(0deg)';
-					this.ui.info[0].innerHTML = 'You will recieve different notifications.<br>' +
+					this.ui.info[0].innerHTML = '<h3 class="tour">Notifications</h3><br>You will recieve different notifications.<br>' +
 												'For example, about your friends activity on Hurri.<br>' +
 												'You can read them by clicking on bell.';
 					this.ui.info[0].style.display = 'block';
@@ -112,7 +112,7 @@ define(['marionette'], function(Marionette){
 					this.ui.arrow[0].style.left = $(document).width() - 600 + 'px';
 					this.ui.arrow[0].style.top = '30px';
 					this.ui.arrow[0].style.transform = 'rotate(0deg)';
-					this.ui.info[0].innerHTML = 'Try search to find your favorite song,<br>' +
+					this.ui.info[0].innerHTML = '<h3 class="tour">Search</h3><br>Try search to find your favorite song,<br>' +
 												'artist or album in Hurri music collection.';
 					this.ui.info[0].style.display = 'block';
 					this.ui.info[0].style.left = $(document).width() - 900 + 'px';
@@ -124,7 +124,7 @@ define(['marionette'], function(Marionette){
 					this.ui.arrow[0].style.left = $(document).width() - 100 + 'px';
 					this.ui.arrow[0].style.top = '30px';
 					this.ui.arrow[0].style.transform = 'rotate(0deg)';
-					this.ui.info[0].innerHTML = 'You can get this help information anytime.<br>' +
+					this.ui.info[0].innerHTML = '<h3 class="tour">Info</h3><br>You can get this help information anytime.<br>' +
 												'Just click here if you have any questions';
 					this.ui.info[0].style.display = 'block';
 					this.ui.info[0].style.left = $(document).width() - 450 + 'px';
