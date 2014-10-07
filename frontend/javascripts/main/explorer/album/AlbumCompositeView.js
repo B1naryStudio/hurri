c82bf18afbf665af068264f9a96bf03ed83463a5
BogdanRusinka <rusinka.bogdann@mail.ru>
2014-10-07 21:55:42 +0300

    Add buttons to play and share album

--- frontend/javascripts/main/explorer/album/AlbumCompositeView.js
+++ frontend/javascripts/main/explorer/album/AlbumCompositeView.js
@@ -1,14 +1,21 @@
-define(['marionette', '../../../shared/songlistmain/MainSongView','../../../sidebar/songlist/Behavior'], 
-	function(Marionette, MainSongView, behavior){
+define(['marionette', '../../../shared/songlistmain/MainSongView','../../../sidebar/songlist/Behavior', 'clipboard'], 
+	function(Marionette, MainSongView, behavior, ZeroClipboard){
 	var AlbumCompositeView = Marionette.CompositeView.extend({
 		className: 'main-explorer-navi',
 		childView: MainSongView,
 		template: '#album-composite-template',
 		events: {
-			'click .add-album-to-list': 'addAlbum'
+			'click .append-to-player': 'addAlbum',
+			'click .playnow-album' : 'playAlbum'
+		},
+		ui : {
+			share : '.share-album'
 		},
 		addAlbum: function(){
 			Backbone.trigger('add-album-to-playlist', this.collection.models);
+		},
+		playAlbum: function(){
+			Backbone.trigger('main-view:play-songs', this.model.attributes._id, this.collection);
 		},
 		childEvents: {
 			'song-view:play-collection' : function(info, models, position){
@@ -16,6 +23,11 @@ define(['marionette', '../../../shared/songlistmain/MainSongView','../../../side
 				Backbone.trigger('song-view:play-song', models, position, this.model.attributes._id);
 			}
 			
+		},
+		onShow: function(){
+			ZeroClipboard.config( { moviePath: '../../../bower_components/zeroclipboard/dist/ZeroClipboard.swf',
+									trustedDomains: location.host } );
+			this.client = new ZeroClipboard( this.ui.share);
 		}
 	});
 	return AlbumCompositeView;
